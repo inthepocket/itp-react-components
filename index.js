80e1bfa95b9adc15eb1e9647dfd65ddc04ded634
Brecht Omey <brecht@brec.ht>
2018-08-22 09:12:16 +0200

    createReactApp, project structure, local packages

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const chalk = require('chalk');
 const Ora = require('ora');
 const fs = require('fs');
 const path = require('path');
-const createPackageJSON = require('./lib/createPackageJSON');
+const createReactApp = require('./lib/createReactApp');
+const cleanupReactApp = require('./lib/cleanupReactApp');
+const updatePackageJSON = require('./lib/updatePackageJSON');
 const copyTemplates = require('./lib/copyTemplates');
 const installNPMPackages = require('./lib/installNPMPackages');
+const initGit = require('./lib/initGit');
 
 const appName = process.argv[2];
 const appDir = `${process.cwd()}/${appName}`;
@@ -22,43 +25,103 @@ const run = async() => {
   console.log('Learn more about create-itp-react-app at https://github.com/');
   console.log(' ');
 
-  // create application dir
+  // remove application dir
   shell.rm('-rf', appDir);
-  shell.mkdir(appDir);
+
+  // create React App
+  console.log(chalk.inverse('Create React App'));
+  await createReactApp({ appName });
+  //spinner.stopAndPersist({ symbol: '✓' });
+
   shell.cd(appDir);
+
+  // cleanup React App
+  spinner.text = chalk.inverse('cleanup React App');
+  spinner.start();
+  await cleanupReactApp({ appDir });
+  spinner.stopAndPersist({ symbol: '✓' });
 
   // copy templates
   spinner.text = 'copying templates';
   spinner.start();
-  copyTemplates({ appDir, templateDir });
+  await copyTemplates({ appDir, templateDir });
   spinner.stopAndPersist({ symbol: '✓' });
 
-  // create package.json
-  spinner.text = 'creating package.json';
-  spinner.start();
-  await createPackageJSON({ appName, overridesDir });
-  spinner.stopAndPersist({ symbol: '✓' });
+  // update local packages package.json
+  await updatePackageJSON({
+    appDir: `${appDir}/src/common`,
+    updateJSON: packageJSON => ({
+      ...packageJSON,
+      name: `${appName}-common`,
+    }),
+  });
+
+  await updatePackageJSON({
+    appDir: `${appDir}/src/core`,
+    updateJSON: packageJSON => ({
+      ...packageJSON,
+      name: `${appName}-core`,
+    }),
+  });
+
+  // install docz
+  console.log(chalk.inverse('Installing docs'));
+  await updatePackageJSON({
+    appDir: `${appDir}/docs`,
+    updateJSON: packageJSON => ({
+      ...packageJSON,
+      name: `${appName}-docs`,
+    }),
+  });
+
+  // install docz packages
+  await installNPMPackages({
+    appDir,
+    dir: path.join(appDir, 'docs'),
+    npmPackages: [
+      'docz',
+      'react',
+      '../src/common',
+    ],
+  });
 
   // install npm packages
-  spinner.text = 'installing npm packages';
-  spinner.start();
+  console.log(chalk.inverse('Installing npm packages'));
   await installNPMPackages({
+    appDir,
+    dir: appDir,
     npmPackages: [
-      'react',
-      'react-dom',
       //'itp-react-scripts',
+      'prop-types',
       'redux',
       'react-redux',
       'redux-saga',
-      'docz',
+      'src/common',
+      'src/core',
     ],
+  });
+
+  // update package.json
+  spinner.text = 'updating package.json';
+  spinner.start();
+  await updatePackageJSON({
+    appDir,
+    updateJSON: packageJSON => ({
+      ...packageJSON,
+      author: 'In The Pocket',
+      license: 'MIT',
+      scripts: {
+        ...packageJSON.scripts,
+        'docz:start': 'cd docs && npm run start',
+        'docz:build': 'cd docs && npm run build',
+      },
+    }),
   });
   spinner.stopAndPersist({ symbol: '✓' });
 
-  // install design system
-  spinner.text = 'installing design system';
-  spinner.start();
-  spinner.stopAndPersist({ symbol: '✓' });
+  // init git
+  console.log(chalk.inverse('Initializing git'));
+  await initGit();
 
   console.log(' ');
   console.log('All set. Happy coding 🚀');
