b22f8e43902f6371b3966fd3b7872f155ea756c6
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2018-09-13 09:25:07 +0200

    remove styledComponents and add defaultStyle to design-docs

--- packages/design-docs/postcss.config.js
+++ packages/design-docs/postcss.config.js
@@ -1,9 +1,32 @@
-/* eslint-disable global-require, import/no-extraneous-dependencies */
+/* eslint-disable global-require, import/no-extraneous-dependencies, import/no-dynamic-require */
+const path = require('path')
+
+const pathToDefaultStyle = `${path.resolve(__dirname)}/root.json`
+let defaultStyle
+
+try {
+  defaultStyle = require(pathToDefaultStyle)
+} catch (e) {
+  defaultStyle = {
+    'color-primary': '#000',
+    'color-secondary': '#fff',
+    'color-tertiary': '#10069f',
+  }
+}
+
+// REMOVE-THIS-LATER overwrite with defaults for now..
+defaultStyle = {
+  'color-primary': '#000',
+  'color-secondary': '#fff',
+  'color-tertiary': '#10069f',
+}
+// REMOVE-THIS-LATER end of overwrite with defaults for now..
+
 module.exports = {
   plugins: [
     require('postcss-custom-properties')({
       appendVariables: true,
-      variables: require('./src/themes/variables.json'),
+      variables: defaultStyle,
     }),
     require('postcss-import')({
       plugins: [require('stylelint')],
