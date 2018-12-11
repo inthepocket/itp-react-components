5d389e08351d0619acc5a6f1abdc58686f10e726
Brecht Omey <brecht@brec.ht>
2018-12-11 08:16:12 +0100

    itp-rcc-card: shared interfaces

---
+++ packages/itp-rcc-card/src/CardChildPropsInterface.ts
@@ -0,0 +1,3 @@
+export default interface CardChildPropsInterface {
+  children: React.ReactNode;
+}
---
+++ packages/itp-rcc-card/src/CardStylesInterface.ts
@@ -0,0 +1,5 @@
+export default interface CardStylesInterface {
+  card: string;
+  body: string;
+  [key: string]: string | undefined;
+}
