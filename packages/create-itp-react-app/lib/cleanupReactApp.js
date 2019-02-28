4cc4318fccd7500baa3a2a31e53ef162a5d04b4d
Brecht Omey <brecht@brec.ht>
2019-02-28 09:15:14 +0100

    cira: enforce typescript

--- packages/create-itp-react-app/lib/cleanupReactApp.js
+++ packages/create-itp-react-app/lib/cleanupReactApp.js
@@ -10,12 +10,9 @@ module.exports = ({ appDir }) =>
   new Promise(resolve => {
     shell.rm([
       path.join(appDir, 'README.md'),
-      path.join(appDir, 'src', 'App.css'),
-      path.join(appDir, 'src', 'App.js'),
-      path.join(appDir, 'src', 'App.test.js'),
+      path.join(appDir, 'src', 'index.*'),
+      path.join(appDir, 'src', 'App.*'),
       path.join(appDir, 'src', 'logo.svg'),
-      path.join(appDir, 'src', 'index.css'),
-      path.join(appDir, 'src', 'index.js'),
       path.join(appDir, 'public', 'favicon.ico'),
     ]);
 
