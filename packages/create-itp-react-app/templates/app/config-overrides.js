86a1e4bced469c92ff9324701a477300c2db3f52
Brecht Omey <brecht@brec.ht>
2019-03-01 10:14:15 +0100

    cira: remove css modules rewiring (comes with cra)

--- packages/create-itp-react-app/templates/app/config-overrides.js
+++ packages/create-itp-react-app/templates/app/config-overrides.js
@@ -1,19 +1,11 @@
-const rewireCssModules = require('react-app-rewire-css-modules-extensionless');
 const rewirePostCss = require('react-app-rewire-postcss');
 
 module.exports = {
-  webpack: (config, env) => {
-    let newConfig = rewireCssModules.webpack(config, env, {
-      test: /\.module.css$/
-    });
-
-    newConfig = rewirePostCss(newConfig, {
-      plugins: loader => [ // eslint-disable-line no-unused-vars
-        require('postcss-preset-env')(), // eslint-disable-line global-require
-      ],
-    });
-
-    return newConfig;
-  },
-  jest: config => rewireCssModules.jest(config),
+  webpack: (config, env) => rewirePostCss(config, {
+    plugins: loader => [ // eslint-disable-line no-unused-vars
+      require('postcss-preset-env')({
+        stage: 1,
+      }), // eslint-disable-line global-require
+    ],
+  }),
 };
